6937bb54fe5d17ab04f80b80cf7ff845d29dd04c
Emanuel <lucasemanuelsena@outlook.com>
2026-03-23 23:33:17 -0300

    Criação do arquivo de equipamento

---
+++ device/equipamento.go
@@ -0,0 +1,42 @@
+package main
+import(
+
+ "rota-das-coisas/shared"
+ "math/rand"
+)
+
+
+func NovoEquipamento(id int, nome string, tipo shared.TipoEquipamento, tempMin float64, tempMax float64) shared.Equipamento  {
+	equip := shared.Equipamento{
+	ID: id,
+	Nome: nome,
+	Tipo: tipo,
+	TempAtual: tempMin + (tempMax-tempMin)/2,
+	TempMin: tempMin,
+	TempMax: tempMax,
+	Status: shared.Normal,
+	Ligado: true,
+}
+	return equip
+}
+
+func AtualizarStatus(equip *shared.Equipamento){
+	if !equip.Ligado {
+		equip.Status = shared.Desligado
+	}else if equip.TempAtual > equip.TempMax || equip.TempAtual < equip.TempMin {
+    equip.Status = shared.Critico
+	}else if equip.TempAtual >= equip.TempMax * 0.9 {
+    equip.Status = shared.Alerta
+	}else{
+	equip.Status = shared.Normal
+} 	
+}
+
+func SimularTemperatura(equip *shared.Equipamento){
+	if equip.Ligado {
+		equip.TempAtual += rand.Float64()*0.2 - 0.1
+	}else{
+		equip.TempAtual += 0.1
+	}
+	
+}
